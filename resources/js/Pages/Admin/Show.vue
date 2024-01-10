af6894d27a8f0283c63a083fbe2c94d975d225f5
Ilham Mulia <dev.ilhammulia@gmail.com>
2024-01-10 22:27:30 +0700

    feat: create user service

---
+++ resources/js/Pages/Admin/Show.vue
@@ -0,0 +1,14 @@
+<template>
+
+  <div>
+    <Button />
+  </div>
+</template>
+
+<script>
+import Button from '@/Components/Button.vue';
+
+export default {
+  components: { Button }
+}
+</script>
---
+++ services/UserService.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Services;
+
+use App\Actions\Fortify\CreateNewUser;
+
+class UserService
+{
+  public function register()
+  {
+  }
+
+  public function login()
+  {
+  }
+
+  public function getUser($id)
+  {
+  }
+
+  public function update($id, $input)
+  {
+  }
+}
