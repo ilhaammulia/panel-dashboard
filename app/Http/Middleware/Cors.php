79f6fd7c8b88bc76618ce63b69abd45472df07d1
Ilham Mulia <dev.ilhammulia@gmail.com>
2024-02-05 14:21:15 +0700

    fix: reset origin

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -15,8 +15,6 @@ class Cors
      */
     public function handle(Request $request, Closure $next): Response
     {
-        return $next($request)->header('Access-Control-Allow-Origin', '*')
-            ->header('Access-Control-Allow-Methods', '*')
-            ->header('Access-Control-Allow-Headers', '*');
+        return $next($request);
     }
 }
